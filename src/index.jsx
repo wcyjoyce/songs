73b8823e1d75c5ef2db245a576e150f05cf29273
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-04-03 06:33:56 +0800

    packages and files imported, reducers added

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,22 @@
-// external modules
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, combineReducers } from "redux";
 
-// internal modules
-import App from './components/app';
-import '../assets/stylesheets/application.scss';
+import App from "./components/app";
+import "../assets/stylesheets/application.scss";
 
-// State and reducers
+import songsReducer from "./reducers/songs_reducer";
+import selectedSongReducer from "./reducers/selected_song_reducer";
+
 const reducers = combineReducers({
-  changeMe: (state = null, action) => state
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
 });
 
-// render an instance of the component in the DOM
 ReactDOM.render(
   <Provider store={createStore(reducers)}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
