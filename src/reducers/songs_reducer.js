18d3fd7949c7504bcd50391e9735f2cfe9b48894
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-04-02 08:13:27 +0800

    manually created song list through reducer

---
+++ src/reducers/songs_reducer.js
@@ -0,0 +1,17 @@
+const songsReducer = () => {
+  return [
+    { artist: "The xx", album: "xx", title: "Intro", duration: "2:07" },
+    { artist: "The xx", album: "xx", title: "VCR", duration: "2:57" },
+    { artist: "The xx", album: "xx", title: "Crystalised", duration: "3:21" },
+    { artist: "The xx", album: "xx", title: "Islands", duration: "2:40" },
+    { artist: "The xx", album: "xx", title: "Heart Skipped A Beat", duration: "4:02" },
+    { artist: "The xx", album: "xx", title: "Fantasy", duration: "2:38" },
+    { artist: "The xx", album: "xx", title: "Shelter", duration: "4:30" },
+    { artist: "The xx", album: "xx", title: "Basic Space", duration: "3:08" },
+    { artist: "The xx", album: "xx", title: "Infinity", duration: "5:13" },
+    { artist: "The xx", album: "xx", title: "Night Time", duration: "3:36" },
+    { artist: "The xx", album: "xx", title: "Stars", duration: "4:22" },
+  ];
+};
+
+export default songsReducer;
